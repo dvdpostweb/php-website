ad1cfac48e25c206fc5c08890cd78faffbec80f4
guillaume <gs@dvdpost.be>
2012-04-25 10:59:54 +0200

    hack chronicles

--- chronicle.php
+++ chronicle.php
@@ -1,3 +1,14 @@
 <?php
-header('Location: http://public.dvdpost.com/'.$_GET['short'].'/chronicles/'.$_GET['id']); 
+if($_GET['test']=='1')
+{
+	$url = 'http://staging.public.dvdpost.com/'.$_GET['lang'].'/chronicles/'.$_GET['id'];
+}
+else
+{
+	$url = 'http://public.dvdpost.com/'.$_GET['lang'].'/chronicles/'.$_GET['id'];
+}
+#echo $url;
+#echo ' <a href="'.$url.'">ici</a>';
+header('Location: '.$url );	
+
 ?>
