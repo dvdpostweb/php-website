1e7ccbd301279d155a3ceb62c020e691d1ae883a
guillaume <gs@dvdpost.be>
2012-06-20 16:05:46 +0200

    new landings flair fda cami ..

--- default.php
+++ default.php
@@ -32,7 +32,7 @@ include(DIR_WS_INCLUDES . 'translation.php');
 $breadcrumb->add(NAVBAR_TITLE, tep_href_link($current_page_name, '', 'NONSSL'));
 
 $page_body_to_include = $current_page_name;
-if($_SERVER['SERVER_NAME'] == 'ptg.dvdpost.be' || $_SERVER['SERVER_NAME'] == 'ptgil.dvdpost.be' || $_SERVER['SERVER_NAME'] == 'groupon.dvdpost.be' || $_SERVER['SERVER_NAME'] == 'cinefriends.dvdpost.be' || $_SERVER['SERVER_NAME'] == 'tryus.dvdpost.be' || $_SERVER['SERVER_NAME'] == 'tryusagain.dvdpost.be'|| $_SERVER['SERVER_NAME'] == 'clubdusoir.dvdpost.be' || $_SERVER['SERVER_NAME'] == 'ipad.dvdpost.be' )
+if($_SERVER['SERVER_NAME'] == 'ptg.dvdpost.be' || $_SERVER['SERVER_NAME'] == 'ptgil.dvdpost.be' || $_SERVER['SERVER_NAME'] == 'groupon.dvdpost.be' || $_SERVER['SERVER_NAME'] == 'cinefriends.dvdpost.be' || $_SERVER['SERVER_NAME'] == 'tryus.dvdpost.be' || $_SERVER['SERVER_NAME'] == 'tryusagain.dvdpost.be'|| $_SERVER['SERVER_NAME'] == 'clubdusoir.dvdpost.be' || $_SERVER['SERVER_NAME'] == 'ipad.dvdpost.be' || $_SERVER['SERVER_NAME'] == 'cami.dvdpost.be' || $_SERVER['SERVER_NAME'] == 'nina.dvdpost.be' || $_SERVER['SERVER_NAME'] == 'shedeals.dvdpost.be' || $_SERVER['SERVER_NAME'] == 'fda.dvdpost.be' || $_SERVER['SERVER_NAME'] == 'flair.dvdpost.be' )
 {
 	
 	include(getBestMatchToInclude(DIR_WS_COMMON . 'canvas/jacob_canvas_light.php',0,$jacob));	
--- pages/default_flair.dvdpost.be.php
+++ pages/default_flair.dvdpost.be.php
@@ -1,14 +1,16 @@
 <? $short = $lang_short == 'en' ? 'fr' : $lang_short ;?>
 <!--   ==============   END HEADER   ==============   -->
 <link href="stylesheet/public_2009.css" media="all" rel="stylesheet" type="text/css" />
-<link href="stylesheet/nina.css" media="all" rel="stylesheet" type="text/css" />
+<link href="stylesheet/flair.css" media="all" rel="stylesheet" type="text/css" />
 <link href="stylesheet/cinefriends.css" media="all" rel="stylesheet" type="text/css" />
-  <div class="promo_content">
-    <p class="promo-title"><img  src="images/fr/enfantparadis_text.png" width="644" height="223" border="0" /></p>
+
+  <div id="promo_content">
+    <p class="promo-title"><img  src="images/<?= $short ?>/flair_text.png" width="522" height="223" border="0" /></p>
     <div class="offre_ptg">
       <form action="activation_code_confirm.php" method="post" name="form1">
         <div id="promo_code">
-<div style="float:left; padding-top:7px;padding-left:27px;font-size:15px">CODE</div>          <div style="float:right;">
+          <div style="float:left; padding-top:10px;padding-left:27px;font-size:15px">CODE</div>
+          <div style="float:right;">
             <input type="text" size="20" value="" class="inputs_codes_groupon" id="code" name="code">
             <input type="image" border="0" align="absmiddle"  src="images/cami_button_go.gif" name="imageField" class="no_border_button">
           </div>
@@ -18,10 +20,11 @@
       </form>
       <div id="applestuffs">
       <p><img src="images/apple.gif" /><br />
-        <?= TEXT_COMPATIBLE_IPAD2 ?><br />
+        <?= TEXT_COMPATIBLE_IPAD ?><br />
 <a href="http://public.dvdpost.com/<?= $short ?>/streaming_products/faq" target="_blank"><?= TEXT_MORE ?></a></p>
         </div>
     </div>
   </div>
+</div>
 </div>
 <p id="footer_promo" align="right">* Via Air Play | <a href="/conditions.php" target="_blank"><strong><?= TEXT_CONDITION_SUMMER ?></strong></a></p>
