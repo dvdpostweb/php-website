89923bec007ef77c1b8ab27753591420eafed682
guillaume <gs@dvdpost.be>
2012-06-25 12:33:05 +0200

    nina debug

--- configure/configure_nina.dvdpost.be.php
+++ configure/configure_nina.dvdpost.be.php
@@ -6,6 +6,6 @@ $constants['WEB_SITE'] = 'nina';
 $constants['WEB_SITE_ID'] = '40';
 $constants['SITE_HOST_ID']= '40';
 
-$constants['DEFAULT_LANGUAGE'] = 'nl';
+$constants['DEFAULT_LANGUAGE'] = 'fr';
 
 ?>
