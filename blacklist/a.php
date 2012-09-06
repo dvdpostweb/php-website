3c3cd4212afe3a6b1b2792a1633175573a569d12
guillaume <gs@dvdpost.be>
2012-09-06 11:11:43 +0200

    test video html5

--- blacklist/a.php
+++ blacklist/a.php
@@ -1,5 +1,12 @@
 <html>
 	<body>
 		<video src='http://94.139.63.189/50485f70cb9a76.62639719_fre_non.m3u8' />
+		
+		<video poster="http://content.bitsontherun.com/thumbs/nfSyO85Q-320.jpg" height="270" width="480" controls="">
+		    <source src="http://devimages.apple.com/iphone/samples/bipbop/gear1/prog_index.m3u8" type="application/vnd.apple.mpegurl">
+		    <source src="http://content.bitsontherun.com/videos/nfSyO85Q-27m5HpIu.webm" type="video/webm">
+		    <source src="http://content.bitsontherun.com/videos/nfSyO85Q-52qL9xLP.mp4" type="video/mp4">
+		    <p class="warning">Your browser does not support HTML5 video.</p>
+		</video>
 	</body>
 </html>
