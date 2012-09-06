bdc7598b89bd891edbdea884d682a2149fd1e400
guillaume <gs@dvdpost.be>
2012-09-06 17:20:29 +0200

    test youtube

--- blacklist/b.php
+++ blacklist/b.php
@@ -1,5 +1,6 @@
 <html>
 	<body>
 		<a href="http://youtu.be/h85gZmsB80A">lien youtube</a>
+		<a href="http://www.dailymotion.com/video/xg39ou_trailer-batman-arkham-city_videogames">lien daily</a>
 	</body>
 </html>
