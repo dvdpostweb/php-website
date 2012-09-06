6fce39494e471e400658df2ec4614030eb554db4
guillaume <gs@dvdpost.be>
2012-09-06 11:20:53 +0200

    test video html5

--- blacklist/a.php
+++ blacklist/a.php
@@ -2,8 +2,6 @@
 	<body>
 		<video poster="http://content.bitsontherun.com/thumbs/nfSyO85Q-320.jpg" height="270" width="480" controls="">
 		    <source src="http://devimages.apple.com/iphone/samples/bipbop/gear1/prog_index.m3u8" type="application/vnd.apple.mpegurl">
-		    <source src="http://content.bitsontherun.com/videos/nfSyO85Q-27m5HpIu.webm" type="video/webm">
-		    <source src="http://content.bitsontherun.com/videos/nfSyO85Q-52qL9xLP.mp4" type="video/mp4">
 		    <p class="warning">Your browser does not support HTML5 video.</p>
 		</video>
 	</body>
