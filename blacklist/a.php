1f55b760c83103925ffed5e7eb045fc765f155ac
guillaume <gs@dvdpost.be>
2012-09-06 11:19:51 +0200

    test video html5

--- blacklist/a.php
+++ blacklist/a.php
@@ -1,8 +1,5 @@
 <html>
 	<body>
-		video 1
-		<video src='http://94.139.63.189/50485f70cb9a76.62639719_fre_non.m3u8' />
-		<br />video 2<br />
 		<video poster="http://content.bitsontherun.com/thumbs/nfSyO85Q-320.jpg" height="270" width="480" controls="">
 		    <source src="http://devimages.apple.com/iphone/samples/bipbop/gear1/prog_index.m3u8" type="application/vnd.apple.mpegurl">
 		    <source src="http://content.bitsontherun.com/videos/nfSyO85Q-27m5HpIu.webm" type="video/webm">
