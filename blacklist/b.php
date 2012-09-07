4e113755932a0d32e26b1a1bf2abb78ece269df4
guillaume <gs@dvdpost.be>
2012-09-07 10:35:50 +0200

    test html5

--- blacklist/b.php
+++ blacklist/b.php
@@ -1,6 +1,10 @@
 <html>
 	<body>
-		<a href="http://youtu.be/h85gZmsB80A">lien youtube</a>
+		<div style="width:100%">
+		<a href="http://youtu.be/h85gZmsB80A">lien youtube</a><br />
 		<a href="http://www.dailymotion.com/video/xg39ou_trailer-batman-arkham-city_videogames">lien daily</a>
+		<br />
+		<video class="video-stream html5-main-video" x-webkit-airplay="allow" src="http://o-o---preferred---lhr07s15---v22---lscache2.c.youtube.com/videoplayback?upn=8ocF1Pq60M8&amp;sparams=cp%2Cgcr%2Cid%2Cip%2Cipbits%2Citag%2Cratebypass%2Csource%2Cupn%2Cexpire&amp;fexp=909519%2C915101%2C906437%2C922401%2C920704%2C912806%2C924412%2C913558%2C912706&amp;key=yt1&amp;itag=43&amp;ipbits=8&amp;signature=16CAA3E8D6AA2C491DEDCC314BABC71668600E81.9B38D06F7F2145C2C064028F43821F5F014F895D&amp;mv=m&amp;sver=3&amp;mt=1347006792&amp;ratebypass=yes&amp;source=youtube&amp;ms=au&amp;gcr=be&amp;expire=1347031003&amp;ip=94.139.62.121&amp;cp=U0hTTFBOT19FTUNOM19ISFNBOmRTR1otWVBuUnhQ&amp;id=289cad21108092a3" mozstereomode="1" data-youtube-id="KJytIRCAkqM" style="width: 640px; height: 266px; left: 0px; top: 47px; "></video>
+		</div>
 	</body>
 </html>
