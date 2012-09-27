efcc62b807c4dab67a8c16f0314d7772ca989263
guillaume <gs@dvdpost.be>
2012-09-27 15:49:27 +0200

    sponsor 300

--- menu.php
+++ menu.php
@@ -1,4 +1,18 @@
 <? include(DIR_WS_INCLUDES . 'functions/language_switcher.php'); ?>
+<?
+function curPageURL() {
+ $pageURL = 'http';
+ if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
+ $pageURL .= "://";
+ if ($_SERVER["SERVER_PORT"] != "80") {
+  $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+ } else {
+  $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+ }
+ return $pageURL;
+}
+?>
+?>
 <? $url = curPageURL() ?>
 <div id="top">
 	<div class="wrap">
