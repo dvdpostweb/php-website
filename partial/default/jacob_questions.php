a27afce99843358b3faf19ca562780263ca735e7
guillaume <gs@dvdpost.be>
2013-02-19 16:32:25 +0100

    change order

--- partial/default/jacob_questions.php
+++ partial/default/jacob_questions.php
@@ -6,6 +6,8 @@
   <? } ?>
   <h3 class="ttl"><?= TEXT_MENU_JACOB ?></h3>
 	<div class="banner_help" align='center'><img src="/images/jb/womanguide.jpg" /></div>
+	<div class="nav_<?= $lang_short ?> question nav" id='question6'><?= HELP_FREE ?></div>
+	<div id="response_color6"><div  class="text_guide" id='response6' style='display:none'><?= RESPONSE4 ?></div></div>
 	<div class="nav_<?= $lang_short ?> question nav" id='question1'><?= HELP_HOW ?></div>
 	<div id="response_color1">
 		<div  class="text_guide" id='response1' style='display:none'><?= RESPONSE1 ?></div>
@@ -21,5 +23,4 @@
 	<div id="response_color4"><div  class="text_guide" id='response4' style='display:none'><?= RESPONSE9 ?></div></div>
 	<div class="nav_<?= $lang_short ?> question nav" id='question5'><?= HELP_CONTACT ?></div>
 	<div id="response_color5"><div  class="text_guide" id='response5' style='display:none'><?= str_replace('{{url}}','contact_public.php',RESPONSE7) ?></div></div>
-	<div class="nav_<?= $lang_short ?> question nav" id='question6'><?= HELP_FREE ?></div>
-	<div id="response_color6"><div  class="text_guide" id='response6' style='display:none'><?= RESPONSE4 ?></div></div>
+
