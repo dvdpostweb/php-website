3c5402a52053cfb199b9e454f8601d6875036759
guillaume <gs@dvdpost.be>
2013-03-05 16:45:02 +0100

    new image

--- promotions/canvas2.php
+++ promotions/canvas2.php
@@ -81,7 +81,7 @@
         <?= TEXT_PROMO_DVD_TITLE ?>
         <table cellpadding="0" cellspacing="0" border="0">
           <tr id="dvd">
-            <td><img src="/images/promotions/dvd.jpg" /></td>
+            <td><img src="/images/promotions/<?= $lang_short ?>/dvd.jpg" /></td>
             <td><img src="/images/promotions/letter.jpg"  style="padding-top:28px;" /></td>
             <td><img src="/images/promotions/tv_dvd.jpg" style="padding-top:28px;" /></td>
             <td><img src="/images/promotions/poste.jpg" style="padding-top:14px;"/></td>
