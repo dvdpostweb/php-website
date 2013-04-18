bee0b608d2bfb488c4d052df8a7f4c6b4ca08723
Christophe <cl@dvdpost.be>
2013-04-18 14:49:50 +0200

    crea landing 2moisfst

---
+++ promotions/2moisfst.php
@@ -0,0 +1,15 @@
+<?
+$canvas = 2;
+$nav=true;
+$image = 'bg_promo_2mois.jpg';
+$code = '2MOISFST';
+$css = 'canvas2.css';
+$date_limit = '02/05/2013';
+#$promo = "2MOIS";
+#$dvd_hide = 1;
+$hide_menu = true;
+$old = true;
+define('DEFAULT_LANGUAGE', 'fr');
+
+require 'header.php'
+?>
