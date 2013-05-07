cfa38a19b4ee7b1182bb59ed736540e1b6493785
Christophe <cl@dvdpost.be>
2013-05-07 11:51:32 +0200

    9 nouvelles landings ANDR

---
+++ promotions/andrfoc.php
@@ -0,0 +1,15 @@
+<?
+$canvas = 2;
+$nav=true;
+$image = 'filmsfst.jpg';
+$code = 'ANDRFOC';
+$css = 'canvas2.css';
+$date_limit = '16/05/2013';
+#$promo = "2MOIS";
+#$dvd_hide = 1;
+$hide_menu = true;
+$old = true;
+define('DEFAULT_LANGUAGE', 'fr');
+
+require 'header.php'
+?>
---
+++ promotions/andrnst.php
@@ -0,0 +1,15 @@
+<?
+$canvas = 2;
+$nav=true;
+$image = 'filmsnst.jpg';
+$code = 'ANDRNST';
+$css = 'canvas2.css';
+$date_limit = '16/05/2013';
+#$promo = "2MOIS";
+#$dvd_hide = 1;
+$hide_menu = true;
+$old = true;
+define('DEFAULT_LANGUAGE', 'nl');
+
+require 'header.php'
+?>
