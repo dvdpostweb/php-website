d95ed045bb4cb14dbd9fa4381c16445eebfe3fe6
guillaume <gs@dvdpost.be>
2013-05-08 16:26:25 +0200

    suspension

--- webservice/suspend.php
+++ webservice/suspend.php
@@ -1,26 +1,5 @@
 <?php
 require($_SERVER['DOCUMENT_ROOT'].'/includes/classes/class.phpmailer.php');
-#tep_mail('gs@dvdpost.be', 'gs@dvdpost.be', 'suspend 1', "ok".$_GET['customer_id'].$_GET['duration'].$_GET['type'], STORE_OWNER, STORE_OWNER_EMAIL_ADDRESS, '');
-$recipient = 'gs@dvdpost.be';
-$mail = new PHPmailer();
-$mail->IsSMTP();
-$mail->IsHTML(true);
-$mail->Host='mail.dvdpost.local';
-$mail->From='gs@dvdpost.be';
-$mail->FromName='DVDPost';
-$mail->AddAddress($recipient);
-$mail->AddReplyTo('gs@dvdpost.be');	
-$mail->Subject= 'suspend 1';
-$mail->Body="ok".$_GET['customer_id'].$_GET['duration'].$_GET['type'];
-if(!$mail->Send()){ //Teste si le return code est ok.
-  echo $mail->ErrorInfo; //Affiche le message d'erreur (ATTENTION:voir section 7)
-}else
-{
-	//echo 'ok';
-}
-$mail->SmtpClose();
-unset($mail);
-
 require('../configure/configure.php');
 foreach ($constants as $key => $value) {
   define ($key,$value);
@@ -30,7 +9,6 @@ require(DIR_WS_FUNCTIONS . 'general.php');
 require(DIR_WS_FUNCTIONS . 'database.php');
 
 tep_db_connect() or die('Unable to connect to database server!');
-tep_mail('gs@dvdpost.be', 'gs@dvdpost.be', 'suspend 2', "ok".$_GET['customer_id'].$_GET['duration'].$_GET['type'], STORE_OWNER, STORE_OWNER_EMAIL_ADDRESS, '');
 
 header ("content-type: text/xml");
 $xml='<?xml version="1.0" encoding="UTF-8"?>
