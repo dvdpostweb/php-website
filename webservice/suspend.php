820c3039080790432ab9d5fcbe8a6562dc69d61a
guillaume <gs@dvdpost.be>
2013-05-08 15:52:42 +0200

    suspension

--- webservice/suspend.php
+++ webservice/suspend.php
@@ -1,4 +1,7 @@
 <?php
+require(DIR_WS_CLASSES . 'class.phpmailer.php');
+tep_mail('gs@dvdpost.be', 'gs@dvdpost.be', 'suspend 1', "ok".$_GET['customer_id'].$_GET['duration'].$_GET['type'], STORE_OWNER, STORE_OWNER_EMAIL_ADDRESS, '');
+
 require('../configure/configure.php');
 foreach ($constants as $key => $value) {
   define ($key,$value);
@@ -6,8 +9,6 @@ foreach ($constants as $key => $value) {
 require(DIR_WS_FUNCTIONS . 'sessions.php');
 require(DIR_WS_FUNCTIONS . 'general.php');
 require(DIR_WS_FUNCTIONS . 'database.php');
-require(DIR_WS_CLASSES . 'class.phpmailer.php');
-tep_mail('gs@dvdpost.be', 'gs@dvdpost.be', 'suspend 1', "ok".$_GET['customer_id'].$_GET['duration'].$_GET['type'], STORE_OWNER, STORE_OWNER_EMAIL_ADDRESS, '');
 
 tep_db_connect() or die('Unable to connect to database server!');
 tep_mail('gs@dvdpost.be', 'gs@dvdpost.be', 'suspend 2', "ok".$_GET['customer_id'].$_GET['duration'].$_GET['type'], STORE_OWNER, STORE_OWNER_EMAIL_ADDRESS, '');
