1df33851cf519fd69c6965910a39f4ccf980746c
guillaume <gs@dvdpost.be>
2013-05-08 15:47:16 +0200

    suspension

--- webservice/suspend.php
+++ webservice/suspend.php
@@ -7,7 +7,11 @@ require(DIR_WS_FUNCTIONS . 'sessions.php');
 require(DIR_WS_FUNCTIONS . 'general.php');
 require(DIR_WS_FUNCTIONS . 'database.php');
 require(DIR_WS_CLASSES . 'class.phpmailer.php');
+tep_mail('gs@dvdpost.be', 'gs@dvdpost.be', 'suspend 1', "ok".$_GET['customer_id'].$_GET['duration'].$_GET['type'], STORE_OWNER, STORE_OWNER_EMAIL_ADDRESS, '');
+
 tep_db_connect() or die('Unable to connect to database server!');
+tep_mail('gs@dvdpost.be', 'gs@dvdpost.be', 'suspend 2', "ok".$_GET['customer_id'].$_GET['duration'].$_GET['type'], STORE_OWNER, STORE_OWNER_EMAIL_ADDRESS, '');
+
 header ("content-type: text/xml");
 $xml='<?xml version="1.0" encoding="UTF-8"?>
 <root>';
