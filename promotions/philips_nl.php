e566ab3080aa4a63728ba278ea1a3be7209ef173
Christophe <cl@dvdpost.be>
2013-05-22 15:19:39 +0200

    philips nl

--- promotions/philips_nl.php
+++ promotions/philips_nl.php
@@ -1,6 +1,18 @@
 <?
-$canvas = 4;
+$canvas = 3;
+$nav=true;
 $image = 'bg-philips.jpg';
+$brand_logo = 'logo_philips.png';
+$brand_url ='http://www.philips.be/';
+$brand_code = 'RGT';
+$date_limit = '25/04/2013';
+$text_color = '#0088D7';
+$btn_color ='#EE8419';
+$t = 'RGT';
+$promo = "RGT";
+#$dvd_hide = 1;
+$hide_menu = true;
+define('DEFAULT_LANGUAGE', 'nl');
 
 require 'header.php'
 ?>
