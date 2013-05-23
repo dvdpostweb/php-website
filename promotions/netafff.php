a5486be9cd4b9c6de97339d08829f639526eda6d
Christophe <cl@dvdpost.be>
2013-05-23 11:53:36 +0200

    2 landings netaffiliation

---
+++ promotions/netafff.php
@@ -0,0 +1,11 @@
+<?
+$canvas = 2;
+$nav=true;
+$image = 'bg_promo_1_month.jpg';
+$code = 'NETAFFF';
+$css = 'canvas2.css';
+$hide_menu = true;
+define('DEFAULT_LANGUAGE', 'fr');
+
+require 'header.php'
+?>
