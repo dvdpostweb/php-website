fedd7a300551fd2cb1ffeb6892fef8ea8abb63ea
Christophe <cl@dvdpost.be>
2013-05-28 11:49:59 +0200

    “”

---
+++ promotions/rougette-nl.php
@@ -0,0 +1,18 @@
+<?
+$canvas = 3;
+$nav=true;
+$image = 'bg-rougette.jpg';
+$brand_logo = 'logo_logo_rougette.png';
+$brand_url ='http://www.rougette.be/';
+$brand_code = 'RGT';
+$date_limit = '25/04/2013';
+$text_color = '#0088D7';
+$btn_color ='#ae0a14';
+#$t = 'RGT';
+$promo = "RGTE";
+#$dvd_hide = 1;
+$hide_menu = true;
+define('DEFAULT_LANGUAGE', 'nl');
+
+require 'header.php'
+?>
---
+++ promotions/rougette.php
@@ -0,0 +1,12 @@
+<?
+$canvas = 4;
+$hide_menu = true;
+$brand_logo = 'logo_logo_rougette.png';
+$image = 'bg_l_rougette.jpg';
+$brand_url ='http://www.rougette.be/';
+$link_fr= 'rougette-fr';
+$link_nl= 'rougette-nl';
+
+
+require 'header.php'
+?>
