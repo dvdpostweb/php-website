fe4744c54c6eac3eec16353e3a29bd6349e0dbfb
Christophe <cl@dvdpost.be>
2013-06-04 09:53:56 +0200

    modif gowie

--- promotions/gwfoc.php
+++ promotions/gwfoc.php
@@ -4,6 +4,7 @@ $nav=true;
 $image = 'bg_promo_1_month1.jpg';
 $date_limit = '03/07/2013';
 $code = 'GWFOC';
+$promo = "1mois1euro";
 $css = 'canvas2.css';
 $hide_menu = true;
 $old = true;
--- promotions/gwnoc.php
+++ promotions/gwnoc.php
@@ -4,6 +4,7 @@ $nav=true;
 $image = 'bg_promo_1_month1.jpg';
 $date_limit = '03/07/2013';
 $code = 'GWNOC';
+$promo = "1mois1euro";
 $css = 'canvas2.css';
 $hide_menu = true;
 $old = true;
