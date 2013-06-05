fe3822eaa448a90cf1633b025bec9b3c7a6c22e5
Christophe <cl@dvdpost.be>
2013-06-05 14:27:22 +0200

    créa vers l'avenir

---
+++ promotions/lavenir.php
@@ -0,0 +1,13 @@
+<?
+$canvas = 2;
+$nav=true;
+$image = 'bg_promo_1_month_avenir.jpg';
+$code = 'VAVENIR';
+$promo = "6films";
+$t = "avenir";
+$css = 'canvas2.css';
+$hide_menu = true;
+define('DEFAULT_LANGUAGE', 'fr');
+
+require 'header.php'
+?>
