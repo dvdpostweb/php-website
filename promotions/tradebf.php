cc2f4aadff5ad68e4a8afdfbec1b1a04b26dc9ce
Christophe <cl@dvdpost.be>
2013-06-10 16:26:55 +0200

    landing tradf

---
+++ promotions/tradebf.php
@@ -0,0 +1,26 @@
+<?
+$canvas = 3;
+$nav=true;
+$image = 'bg_promo_1_month.jpg';
+#$brand_logo = 'logo_philips.png';
+#$brand_url ='http://www.philips.be/';
+#$brand_code = 'RGT';
+#$lienoldcust = '/phone_requests/new';
+#$date_limit = '25/04/2013';
+#$text_color = '#0088D7';
+#$btn_color ='#EE8419';
+#$t = 'RGT';
+#$promo = "RGT";
+$hide_old = true;
+$hide_explain = true;
+$image_promotion1 ="fr/codepromo_RGT.jpg";
+$image_promotion2 ="promo_RGT.jpg";
+$image_promotion3 ="step3_RGT.jpg";
+$image_promotion4 ="step4_RGT.jpg";
+#$dvd_hide = 1;
+$hide_menu = true;
+
+define('DEFAULT_LANGUAGE', 'fr');
+
+require 'header.php'
+?>
