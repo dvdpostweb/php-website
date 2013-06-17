4c666330329c0441ea43b8cc002850f226d829dc
Christophe <cl@dvdpost.be>
2013-06-17 12:35:31 +0200

    landings zalando

---
+++ promotions/dvdpost.php
@@ -0,0 +1,12 @@
+<?
+$canvas = 4;
+$hide_menu = true;
+#$brand_logo = 'logo_philips.png';
+$image = 'bg_l_zalando.jpg';
+#$brand_url ='http://www.philips.be/';
+$link_fr= 'flyer-fr';
+$link_nl= 'flyer-nl';
+
+
+require 'header.php'
+?>
