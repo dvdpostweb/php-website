bf57fe7f3df785d087122667ac40c2257087c996
Christophe <cl@dvdpost.be>
2013-06-19 15:40:47 +0200

    landings countdown

---
+++ promotions/ctdwnn.php
@@ -0,0 +1,16 @@
+<?
+$canvas = 2;
+$nav=true;
+$image = 'bg_countdown.jpg';
+$code = 'CTDWNN';
+$css = 'canvas2.css';
+#$date_limit = '23/06/2013';
+$promo = "50PC";
+$t = "70";
+#$dvd_hide = 1;
+$hide_menu = true;
+$old = true;
+define('DEFAULT_LANGUAGE', 'nl');
+
+require 'header.php'
+?>
