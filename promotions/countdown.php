e56aab0f8107141865341e881df94a489f7a2dd4
Christophe <cl@dvdpost.be>
2013-06-21 11:49:08 +0200

    landings countdown

---
+++ promotions/countdown.php
@@ -0,0 +1,10 @@
+<?
+$canvas = 4;
+$hide_menu = true;
+$image = 'bg_l_rougette.jpg';
+$link_fr= 'ctdwnf';
+$link_nl= 'ctdwnn';
+
+
+require 'header.php'
+?>
---
+++ promotions/flyer-nl.php
@@ -0,0 +1,27 @@
+<?
+$canvas = 3;
+$nav=true;
+$image = 'bg-zalando.jpg';
+$hide_image = true;
+$show_date = true;
+#$brand_logo = 'logo_philips.png';
+#$brand_url ='http://www.philips.be/';
+$hide_old = true;
+#$hide_explain = true;
+#$brand_code = 'RGT';
+#$lienoldcust = '/phone_requests/new';
+#$date_limit = '25/04/2013';
+$text_color = '#FFF';
+$btn_color ='#EE8419';
+#$t = 'RGT';
+#$promo = "RGT";
+$image_promotion1 ="fr/codepromo.jpg";
+$image_promotion2 ="promo.jpg";
+$image_promotion3 ="step3_RGT.jpg";
+$image_promotion4 ="step4_RGT.jpg";
+#$dvd_hide = 1;
+$hide_menu = true;
+define('DEFAULT_LANGUAGE', 'nl');
+
+require 'header.php'
+?>
