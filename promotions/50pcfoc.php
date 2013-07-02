c2fa9d91d847d05d96bd2586b0797c06bd6287e4
Christophe <cl@dvdpost.be>
2013-07-02 16:47:54 +0200

    landings summer

---
+++ promotions/50pcfoc.php
@@ -0,0 +1,17 @@
+<?
+$canvas = 2;
+$nav=true;
+$image = 'bg_countdown.jpg';
+$code = 'SMBFOC';
+$css = 'canvas2.css';
+#$date_limit = '10/07/2013';
+$promo = "50PC";
+$t = "70";
+#$dvd_hide = 1;
+$hide_menu = true;
+#$old = true;
+$btn = 'PC';
+define('DEFAULT_LANGUAGE', 'fr');
+
+require 'header.php'
+?>
