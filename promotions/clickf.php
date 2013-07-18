1c98814237df9b68496f22902ce43a109eb27798
Christophe <cl@dvdpost.be>
2013-07-18 12:15:48 +0200

    modification clicktron

--- promotions/clickf.php
+++ promotions/clickf.php
@@ -2,7 +2,7 @@
 $canvas = 2;
 $nav=true;
 $image = 'bg_promo_1_month.jpg';
-$code = 'CLICKF';
+$code = 'CLICKFB';
 $css = 'canvas2.css';
 $hide_menu = true;
 define('DEFAULT_LANGUAGE', 'fr');
--- promotions/clickn.php
+++ promotions/clickn.php
@@ -2,7 +2,7 @@
 $canvas = 2;
 $nav=true;
 $image = 'bg_promo_1_month.jpg';
-$code = 'CLICKN';
+$code = 'CLICKNB';
 $css = 'canvas2.css';
 $hide_menu = true;
 define('DEFAULT_LANGUAGE', 'nl');
