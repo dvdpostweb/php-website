350b31fc27c9e35ffef4f0c1495bf71197c96159
Christophe <cl@dvdpost.be>
2013-09-23 14:43:21 +0200

    landings ptg

--- promotions/ptgnb.php
+++ promotions/ptgnb.php
@@ -5,7 +5,7 @@ $image = 'bg_promo_4_films.jpg';
 $code = 'PTGNL';
 $css = 'canvas2.css';
 $hide_menu = true;
-define('DEFAULT_LANGUAGE', 'fr');
+define('DEFAULT_LANGUAGE', 'nl');
 
 require 'header.php'
 ?>
