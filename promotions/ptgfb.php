1710d1e91c5f382e49591d20150301026e2a9a58
Christophe <cl@dvdpost.be>
2013-09-23 14:40:54 +0200

    landings ptg

---
+++ promotions/ptgfb.php
@@ -0,0 +1,11 @@
+<?
+$canvas = 2;
+$nav=true;
+$image = 'bg_promo_4_films.jpg';
+$code = 'PTGFR';
+$css = 'canvas2.css';
+$hide_menu = true;
+define('DEFAULT_LANGUAGE', 'fr');
+
+require 'header.php'
+?>
