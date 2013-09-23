8b1a69c1f2a3bf3622a519de8c1c16a518c0f014
Christophe <cl@dvdpost.be>
2013-09-23 14:07:45 +0200

    landing ptg

---
+++ promotions/ptgf.php
@@ -0,0 +1,28 @@
+<?
+$canvas = 3;
+$nav=true;
+$image = 'bg_promo_5_films.jpg';
+$show_date = true;
+$hide_image = true;
+#$brand_logo = 'logo_philips.png';
+#$brand_url ='http://www.philips.be/';
+#$brand_code = 'RGT';
+#$lienoldcust = '/phone_requests/new';
+#$date_limit = '25/04/2013';
+$text_color = '#FFF';
+#$btn_color ='#EE8419';
+#$t = 'RGT';
+$promo = "5FILMS";
+$hide_old = true;
+$hide_explain = true;
+$image_promotion1 ="fr/codepromo_RGT.jpg";
+$image_promotion2 ="promo_RGT.jpg";
+$image_promotion3 ="step3_RGT.jpg";
+$image_promotion4 ="step4_RGT.jpg";
+#$dvd_hide = 1;
+$hide_menu = true;
+
+define('DEFAULT_LANGUAGE', 'fr');
+
+require 'header.php'
+?>
