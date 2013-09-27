95245daa6d6e2859b9050f339e2bb17e94ee4b0e
Christophe <cl@dvdpost.be>
2013-09-27 15:40:34 +0200

    landings lg

--- promotions/lg_fr.php
+++ promotions/lg_fr.php
@@ -1,18 +1,18 @@
 <?
 $canvas = 3;
 $nav=true;
-$image = 'bg-lg.jpg';
+$image = 'bg_promo_lg.jpg';
 $brand_logo = 'logo_lg.png';
 $brand_url ='http://www.lg.com/';
 $brand_code = 'RGT';
 #$lienoldcust = '/phone_requests/new';
 $date_limit = '30/02/2014';
-$text_color = '#0088D7';
+$text_color = '#72797f';
 $btn_color ='#EE8419';
-$t = 'RGT';
+$t = 'LG';
 $promo = "RGT";
 $image_promotion1 ="fr/codepromo_lg.jpg";
-$image_promotion2 ="promo_lg.jpg";
+$image_promotion2 ="promo8vod.jpg";
 $image_promotion3 ="step3_RGT.jpg";
 $image_promotion4 ="step4_RGT.jpg";
 #$dvd_hide = 1;
