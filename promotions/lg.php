d1c3ca3fd755c10092c11d82b77396e08d655d9f
Christophe <cl@dvdpost.be>
2013-09-27 15:12:20 +0200

    landings lg

---
+++ promotions/lg.php
@@ -0,0 +1,12 @@
+<?
+$canvas = 4;
+$hide_menu = true;
+$brand_logo = 'logo_lg.png';
+$image = 'bg_l_philips.jpg';
+$brand_url ='http://www.lg.com/';
+$link_fr= 'lg_fr';
+$link_nl= 'lg_nl';
+
+
+require 'header.php'
+?>
---
+++ promotions/lg_fr.php
@@ -0,0 +1,23 @@
+<?
+$canvas = 3;
+$nav=true;
+$image = 'bg-lg.jpg';
+$brand_logo = 'logo_lg.png';
+$brand_url ='http://www.lg.com/';
+$brand_code = 'RGT';
+#$lienoldcust = '/phone_requests/new';
+$date_limit = '30/02/2014';
+$text_color = '#0088D7';
+$btn_color ='#EE8419';
+$t = 'RGT';
+$promo = "RGT";
+$image_promotion1 ="fr/codepromo_LG8.jpg";
+$image_promotion2 ="promo_LG8.jpg";
+$image_promotion3 ="step3_RGT.jpg";
+$image_promotion4 ="step4_RGT.jpg";
+#$dvd_hide = 1;
+$hide_menu = true;
+define('DEFAULT_LANGUAGE', 'fr');
+
+require 'header.php'
+?>
