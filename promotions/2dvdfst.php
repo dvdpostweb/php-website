e1b656d3898e3b94f2e1de3b76d6d8c32fc154c0
Christophe <cl@dvdpost.be>
2013-10-01 16:10:01 +0200

    landings dvd

---
+++ promotions/2dvdfst.php
@@ -0,0 +1,15 @@
+<?
+$canvas = 2;
+$nav=true;
+$image = 'bg_2dvdfree.jpg';
+$code = '2DVDFST';
+$css = 'canvas2.css';
+$date_limit = '10/10/2013';
+$promo = "2DVD";
+$vod_hide = 1;
+$hide_menu = true;
+#$old = true;
+define('DEFAULT_LANGUAGE', 'fr');
+
+require 'header.php'
+?>
