8fa82402abc606c415158a669a058fbe866902dc
Christophe <cl@dvdpost.be>
2013-10-08 14:57:57 +0200

    landings halloween

---
+++ promotions/halloween.php
@@ -0,0 +1,12 @@
+<?
+$canvas = 4;
+$hide_menu = true;
+$brand_logo = 'logo_samsung.png';
+$image = 'bg_l_samsung_halloween.jpg';
+$brand_url ='http://www.samsung.com/be';
+$link_fr= 'halloween_fr';
+$link_nl= 'halloween_nl';
+
+
+require 'header.php'
+?>
---
+++ promotions/halloween_fr.php
@@ -0,0 +1,25 @@
+<?
+$canvas = 3;
+$nav=true;
+$image = 'bg_promo_samsung_halloween.jpg';
+$brand_logo = 'logo_samsung.png';
+$brand_url ='http://www.samsung.com/be';
+$brand_code = 'SMH';
+$hide_image = 1;
+#$lienoldcust = '/phone_requests/new';
+$date_limit = '30/11/2013';
+$text_color = '#FFFFFF';
+$btn_color ='#EE8419';
+$t = 'SMH';
+$promo = "SMH";
+$image_promotion1 ="fr/codepromo_SHW.jpg";
+$image_promotion2 ="promo1vod.jpg";
+$image_promotion3 ="step3_RGT.jpg";
+$image_promotion4 ="step4_RGT.jpg";
+$dvd_hide = 1;
+$hide_menu = true;
+$hide_old = true;
+define('DEFAULT_LANGUAGE', 'fr');
+
+require 'header.php'
+?>
