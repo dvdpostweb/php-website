73405ebebcc2fdc2616f081a06c3f47271dc0d1e
Christophe <cl@dvdpost.be>
2013-10-08 17:03:31 +0200

    landings halloween

--- promotions/halloween.php
+++ promotions/halloween.php
@@ -1,7 +1,7 @@
 <?
 $canvas = 4;
 $hide_menu = true;
-$brand_logo = 'logo_samsung.png';
+$brand_logo = 'logo_samsung.jpg';
 $image = 'bg_l_samsung_halloween.jpg';
 $brand_url ='http://www.samsung.com/be';
 $link_fr= 'halloween_fr';
