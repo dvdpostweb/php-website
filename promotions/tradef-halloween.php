c4bf98374ffb3aad461fae54641e79e0cbb74b42
Christophe <cl@dvdpost.be>
2013-10-14 14:01:02 +0200

    landings halloween

---
+++ promotions/tradef-halloween.php
@@ -0,0 +1,11 @@
+<?
+$canvas = 2;
+$nav=true;
+$image = 'bg_halloween_4films';
+$code = 'TRADEF';
+$css = 'canvas2.css';
+$hide_menu = true;
+define('DEFAULT_LANGUAGE', 'fr');
+
+require 'header.php'
+?>
