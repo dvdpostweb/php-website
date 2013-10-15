e22bd674602270606a692ce87da1d6affe2d9df1
Christophe <cl@dvdpost.be>
2013-10-15 14:54:24 +0200

    landings 6films

--- promotions/netafff-halloween.php
+++ promotions/netafff-halloween.php
@@ -3,6 +3,7 @@ $canvas = 2;
 $nav=true;
 $image = 'bg_halloween_4films.jpg';
 $code = 'NETAFFF';
+$t = 'AFFH';
 $css = 'canvas2.css';
 $hide_menu = true;
 define('DEFAULT_LANGUAGE', 'fr');
--- promotions/netaffn-halloween.php
+++ promotions/netaffn-halloween.php
@@ -3,6 +3,7 @@ $canvas = 2;
 $nav=true;
 $image = 'bg_halloween_4films.jpg';
 $code = 'NETAFFN';
+$t = 'AFFH';
 $css = 'canvas2.css';
 $hide_menu = true;
 define('DEFAULT_LANGUAGE', 'nl');
