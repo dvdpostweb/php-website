05e621d5ece3cfb0e6aa5e9ca45248feacef7b8b
Christophe <cl@dvdpost.be>
2013-10-21 13:59:15 +0200

    landings 4films

--- promotions/4filmsfoc.php
+++ promotions/4filmsfoc.php
@@ -2,14 +2,14 @@
 $canvas = 2;
 $nav=true;
 $image = 'bg_promo_4_films.jpg';
-$code = 'REAC2NPR';
+$code = 'REAC2FOC';
 $css = 'canvas2.css';
 $date_limit = '31/10/2013';
 #$promo = "8FILMS";
 #$dvd_hide = 1;
 $hide_menu = true;
-#$old = true;
-define('DEFAULT_LANGUAGE', 'nl');
+$old = true;
+define('DEFAULT_LANGUAGE', 'fr');
 
 require 'header.php'
 ?>
