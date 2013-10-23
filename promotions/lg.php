eb8ca051d04d885d6554c9967e7e0ccd9906678d
guillaume <gs@dvdpost.be>
2013-10-23 10:36:38 +0200

    lg

--- promotions/lg.php
+++ promotions/lg.php
@@ -2,7 +2,7 @@
 $canvas = 4;
 $hide_menu = true;
 $brand_logo = 'logo_lg.png';
-$image = 'bg_l_philips.jpg';
+$image = 'bg_l_lg.jpg';
 $brand_url ='http://www.lg.com/';
 $link_fr= 'lg_fr';
 $link_nl= 'lg_nl';
