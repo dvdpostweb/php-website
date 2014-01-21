0f73ec944e5a9d5b3946f5982fc6d12c1ff789aa
Christophe <cl@dvdpost.be>
2014-01-21 16:16:38 +0100

    landing 13 VOD

--- promotions/13vodfpr.php
+++ promotions/13vodfpr.php
@@ -6,7 +6,7 @@ $code = '13VODFPR';
 $css = 'canvas2.css';
 $date_limit = '30/01/2014';
 $promo = "13VOD";
-$vod_hide = 1;
+$dvd_hide = 1;
 $hide_menu = true;
 #$old = true;
 define('DEFAULT_LANGUAGE', 'fr');
