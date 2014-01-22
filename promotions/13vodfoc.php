3ee295a9b403b16cc2074fa92464e3835f956028
Christophe <cl@dvdpost.be>
2014-01-22 12:01:24 +0100

    landing 13 VOD

---
+++ promotions/13vodfoc.php
@@ -0,0 +1,15 @@
+<?
+$canvas = 2;
+$nav=true;
+$image = 'bg_promo_ipad.jpg';
+$code = '13VODFOC';
+$css = 'canvas2.css';
+$date_limit = '30/01/2014';
+$promo = "13VOD";
+$dvd_hide = 1;
+$hide_menu = true;
+$old = true;
+define('DEFAULT_LANGUAGE', 'fr');
+
+require 'header.php'
+?>
---
+++ promotions/13vodnpr.php
@@ -0,0 +1,15 @@
+<?
+$canvas = 2;
+$nav=true;
+$image = 'bg_2dvdfree.jpg';
+$code = '13VODNPR';
+$css = 'canvas2.css';
+$date_limit = '30/01/2014';
+$promo = "13VOD";
+$dvd_hide = 1;
+$hide_menu = true;
+#$old = true;
+define('DEFAULT_LANGUAGE', 'nl');
+
+require 'header.php'
+?>
