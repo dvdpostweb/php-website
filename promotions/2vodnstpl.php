41c504e02288b619ad203f09f61095268a49b795
Christophe <cl@dvdpost.be>
2014-02-04 15:40:35 +0100

    landing 2 VOD

--- promotions/2vodnstpl.php
+++ promotions/2vodnstpl.php
@@ -2,14 +2,14 @@
 $canvas = 2;
 $nav=true;
 $image = 'bg_eone.jpg';
-$code = 'E1DEOC';
+$code = 'E1PNST';
 $css = 'canvas2.css';
 $date_limit = '13/02/2014';
 $promo = "2VOD";
 $dvd_hide = 1;
 $hide_menu = true;
 $old = true;
-define('DEFAULT_LANGUAGE', 'en');
+define('DEFAULT_LANGUAGE', 'nl');
 
 require 'header.php'
 ?>
