e6c00416d47241b3f6dca2c16aa613b0b4dcae29
Christophe <cl@dvdpost.be>
2014-02-05 10:53:16 +0100

    landing 2 VOD

---
+++ promotions/2vodnprpl.php
@@ -0,0 +1,16 @@
+<?
+$canvas = 2;
+$nav=true;
+$image = 'bg_eone.jpg';
+$code = 'E1PNPR';
+$css = 'canvas2.css';
+$date_limit = '13/02/2014';
+$promo = "2VOD";
+$t = 'eone';
+$dvd_hide = 1;
+$hide_menu = true;
+#$old = true;
+define('DEFAULT_LANGUAGE', 'nl');
+
+require 'header.php'
+?>
