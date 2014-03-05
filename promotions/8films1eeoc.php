5ef22f79e184b630a9fe0da8af862b9da569b6bd
Christophe <cl@dvdpost.be>
2014-03-05 14:07:52 +0100

    landing entertain

---
+++ promotions/8films1eeoc.php
@@ -0,0 +1,16 @@
+<?
+$canvas = 2;
+$nav=true;
+$image = 'bg_copain.jpg';
+$code = 'ENTEOC';
+$css = 'canvas2.css';
+$date_limit = '13/03/2014';
+$promo = "8FILMS1E";
+$t = "8FILMS1E";
+#$dvd_hide = 1;
+$hide_menu = true;
+$old = true;
+define('DEFAULT_LANGUAGE', 'en');
+
+require 'header.php'
+?>
---
+++ promotions/8films1enst.php
@@ -0,0 +1,16 @@
+<?
+$canvas = 2;
+$nav=true;
+$image = 'bg_copain.jpg';
+$code = 'ENTNOC';
+$css = 'canvas2.css';
+$date_limit = '13/03/2014';
+$promo = "8FILMS1E";
+$t = "8FILMS1E";
+#$dvd_hide = 1;
+$hide_menu = true;
+$old = true;
+define('DEFAULT_LANGUAGE', 'nl');
+
+require 'header.php'
+?>
