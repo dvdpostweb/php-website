b2d7b8d3f9b55baea8e288160f9023010d01b369
Christophe <cl@dvdpost.be>
2014-03-19 10:09:15 +0100

    landing 4 films

--- promotions/4filmsfpr.php
+++ promotions/4filmsfpr.php
@@ -1,9 +1,9 @@
 <?
 $canvas = 2;
 $nav=true;
-$image = 'bg_stvalentin.jpg';
+$image = 'bg_promo_affi.jpg';
 #$image = 'bg_promo_4_films.jpg';
-$code = 'ROMFPR';
+$code = 'AFMFPR';
 $css = 'canvas2.css';
 $date_limit = '06/03/2014';
 #$promo = "8FILMS";
---
+++ promotions/4vodnoc.php
@@ -0,0 +1,16 @@
+<?
+$canvas = 2;
+$nav=true;
+$image = 'bg_promo_affi.jpg';
+$code = 'AFMNOC';
+$css = 'canvas2.css';
+$date_limit = '27/03/2014';
+$promo = "4VOD";
+#$t = 'eone';
+#$dvd_hide = 1;
+$hide_menu = true;
+$old = true;
+define('DEFAULT_LANGUAGE', 'nl');
+
+require 'header.php'
+?>
