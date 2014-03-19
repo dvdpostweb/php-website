85f21344a1f7a2907be133ee200c4eec0dec5a0e
Christophe <cl@dvdpost.be>
2014-03-19 09:27:37 +0100

    landing 4 films

---
+++ promotions/4vodfoc.php
@@ -0,0 +1,16 @@
+<?
+$canvas = 2;
+$nav=true;
+$image = 'bg_promo_affi.jpg';
+$code = 'AFMFOC';
+$css = 'canvas2.css';
+$date_limit = '27/03/2014';
+$promo = "4VOD";
+#$t = 'eone';
+$dvd_hide = 1;
+$hide_menu = true;
+$old = true;
+define('DEFAULT_LANGUAGE', 'fr');
+
+require 'header.php'
+?>
