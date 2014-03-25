47960e24e4fc52c46b9393ba6dec6c7262ff2553
guillaume <gs@dvdpost.be>
2014-03-25 17:35:51 +0100

    akamai manifest

--- hls-vod/manifest.php
+++ hls-vod/manifest.php
@@ -1,1 +1,9 @@
-<? echo $token; ?>
+#EXTM3U
+#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=800000
+http://dvdpost.akamaihd.net/hls-vode/hls-vod//<? echo $token; ?>_800k.f4v.m3u8
+#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=3000000
+http://dvdpost.akamaihd.net/hls-vod/hls-vod//<? echo $token; ?>_3000k.f4v.m3u8
+#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2200000
+http://dvdpost.akamaihd.net/hls-vod/hls-vod//<? echo $token; ?>_2200k.f4v.m3u8
+#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=1400000
+http://dvdpost.akamaihd.net/hls-vod/hls-vod//<? echo $token; ?>_1400k.f4v.m3u8
