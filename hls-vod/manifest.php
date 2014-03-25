ec122d4ad2a6aa627bc74c8531386b1972a22b85
guillaume <gs@dvdpost.be>
2014-03-25 17:52:06 +0100

    change

--- hls-vod/manifest.php
+++ hls-vod/manifest.php
@@ -4,10 +4,10 @@ header("Content-Disposition: attachment; filename=".$token.".m3u8");
 ?>
 #EXTM3U
 #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=800000
-http://akamai.dvdpost.be/hls-vod//<? echo $token; ?>_800k.f4v.m3u8
+http://akamai.dvdpost.be/hls-vod/<? echo $token; ?>_800k.f4v.m3u8
 #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=3000000
-http://akamai.dvdpost.be/hls-vod//<? echo $token; ?>_3000k.f4v.m3u8
+http://akamai.dvdpost.be/hls-vod/<? echo $token; ?>_3000k.f4v.m3u8
 #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2200000
-http://akamai.dvdpost.be/hls-vod//<? echo $token; ?>_2200k.f4v.m3u8
+http://akamai.dvdpost.be/hls-vod/<? echo $token; ?>_2200k.f4v.m3u8
 #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=1400000
-http://akamai.dvdpost.be/hls-vod//<? echo $token; ?>_1400k.f4v.m3u8
+http://akamai.dvdpost.be/hls-vod/<? echo $token; ?>_1400k.f4v.m3u8
