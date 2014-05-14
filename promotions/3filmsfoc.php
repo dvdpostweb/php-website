7e43e0426cde62e2fc08a6d0f13117eb44c234f7
Christophe <cl@dvdpost.be>
2014-05-14 15:47:05 +0200

    landing 3films

---
+++ promotions/3filmsfoc.php
@@ -0,0 +1,15 @@
+<?
+$canvas = 2;
+$nav=true;
+$image = 'bg_hungergames.jpg';
+$code = 'HUNFOC';
+$css = 'canvas2.css';
+$date_limit = '22/05/2014';
+$promo = "3VOD";
+#$dvd_hide = 1;
+$hide_menu = true;
+$old = true;
+define('DEFAULT_LANGUAGE', 'fr');
+
+require 'header.php'
+?>
