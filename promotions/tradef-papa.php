0417657ad490375c272288e8a07e5ac12656fc4f
Christophe <cl@dvdpost.be>
2014-05-21 14:14:12 +0200

    landing 3films

---
+++ promotions/tradef-papa.php
@@ -0,0 +1,11 @@
+<?
+$canvas = 2;
+$nav=true;
+$image = 'bg_fatherday4.jpg';
+$code = 'TRADEF';
+$css = 'canvas2.css';
+$hide_menu = true;
+define('DEFAULT_LANGUAGE', 'fr');
+
+require 'header.php'
+?>
---
+++ promotions/traden-papa.php
@@ -0,0 +1,11 @@
+<?
+$canvas = 2;
+$nav=true;
+$image = 'bg_fatherday4.jpg';
+$code = 'TRADEN';
+$css = 'canvas2.css';
+$hide_menu = true;
+define('DEFAULT_LANGUAGE', 'nl');
+
+require 'header.php'
+?>
