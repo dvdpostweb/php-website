a6cd20204192463f0655c68872a3bcb225914085
Christophe <cl@dvdpost.be>
2014-06-17 12:53:31 +0200

    landing 4dvd

---
+++ promotions/4dvdfoc.php
@@ -0,0 +1,15 @@
+<?
+$canvas = 2;
+$nav=true;
+$image = 'bg_4dvdfree.jpg';
+$code = '4DVDFOC';
+$css = 'canvas2.css';
+$date_limit = '26/06/2014';
+$promo = "4DVD";
+$vod_hide = 1;
+$hide_menu = true;
+$old = true;
+define('DEFAULT_LANGUAGE', 'fr');
+
+require 'header.php'
+?>
