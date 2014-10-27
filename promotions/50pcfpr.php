729ef1d6f7167ff1969dc02c1654a6d5d8f348a3
guillaume <gs@dvdpost.be>
2014-10-27 16:03:16 +0100

    landings 50pc

--- promotions/50pcfpr.php
+++ promotions/50pcfpr.php
@@ -2,14 +2,14 @@
 $canvas = 2;
 $nav=true;
 $image = 'bg_promo_50_2mois.jpg';
-$code = '50PCFOC';
+$code = '50PCFPR';
 $css = 'canvas2.css';
 $date_limit = '06/11/2014';
 $promo = "50PC";
 $t = "50";
 #$dvd_hide = 1;
 $hide_menu = true;
-$old = true;
+#$old = true;
 $btn = 'PC';
 define('DEFAULT_LANGUAGE', 'fr');
 
