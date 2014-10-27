108793c6de116ab3390c8db373e6949d0b125269
guillaume <gs@dvdpost.be>
2014-10-27 15:47:04 +0100

    landings 50pc

--- promotions/50pcfoc.php
+++ promotions/50pcfoc.php
@@ -2,11 +2,11 @@
 $canvas = 2;
 $nav=true;
 $image = 'bg_promo_50_2mois.jpg';
-$code = 'SMBFOC';
+$code = '50PCFOC';
 $css = 'canvas2.css';
 #$date_limit = '10/07/2013';
 $promo = "50PC";
-$t = "70";
+$t = "50";
 #$dvd_hide = 1;
 $hide_menu = true;
 $old = true;
