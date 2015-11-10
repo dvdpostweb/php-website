a756714beb50ee86161ec763e1b9ad139bb8041a
popac <aleksandar.popovic@codepop.biz>
2015-11-10 16:20:12 +0100

    remove univers code

---
+++ promotions/4films.php
@@ -0,0 +1,16 @@
+<?
+$canvas = 2;
+$nav=true;
+$image = 'bg_hobbit.jpg';
+#$image = 'bg_promo_4_films.jpg';
+$code = 'SPEOC';
+$css = 'canvas2.css';
+#$date_limit = '04/09/2014';
+#$promo = "8FILMS";
+#$dvd_hide = 1;
+$hide_menu = true;
+$old = true;
+define('DEFAULT_LANGUAGE', 'en');
+
+require 'header.php'
+?>
