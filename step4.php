dbdbba3a89d8cc07f7bd808c11756916a47ae199
popac <aleksandar.popovic@codepop.biz>
2015-11-20 17:07:52 +0100

    s

--- step4.php
+++ step4.php
@@ -167,7 +167,7 @@ if (!tep_session_is_registered('customer_id')) {
 		?>
 		<script>
     dataLayer = [{
-      'revenue': '0'
+      'revenue': '<?= $price_abo_next ?>'
     }];
 </script>
 	<!-- Google Tag Manager -->
