21ff0d4c9cfc196241cc993db1bbc285a8732e18
popac <web.guru.serbia@gmail.com>
2017-02-06 17:11:43 +0100

    Set productiondeploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -43,5 +43,27 @@ namespace :deploy do
     #run "cp /data/sites/benelux/php_app/shared/cached-copy/configure/*  #{release_path}/configure/"
     #run "cp /data/sites/benelux/releases/zend/*  #{release_path}/rest/application/"
     #run "chmod 777 -R #{release_path}/webservice/"
+
+    ms_config = <<-EOF
+    <?php 
+	  $constants['MAX_DISPLAY_SEARCH_RESULTS'] = '10';
+	  $constants['MAX_DISPLAY_SEARCH_RESULTS_PRIVATE'] = '30';
+	  $constants['MAX_DISPLAY_ORDER_HISTORY'] = '20';
+	  $constants['ENVIRONMENT'] = '';
+	  $constants['DIR_WS_COMMON'] = '/Users/gs/site_local/trunk/';
+	  $constants['DIR_FS_DOCUMENT_ROOT']= '/Users/gs/site_local/trunk/';
+	  $constants['ADMINIP']= '192.168.100.254';
+	  $constants['DB_SERVER']= '192.168.100.204';
+	  $constants['DB_SERVER_RO']= '192.168.100.205';
+	  $constants['DB_REDIRECT_ALL_RO'] = 'true';
+	  $constants['DB_SERVER_USERNAME']= 'webuser';
+	  $constants['DB_SERVER_PASSWORD']= '3gallfir-';
+	  $constants['DB_DATABASE']= 'dvdpost_be_prod';
+	  $constants['USE_PCONNECT']= 'false';  
+	  $constants['STORE_SESSIONS']= '';
+	?>
+    EOF
+    put ms_config, "#{release_path}/configure/configure_machine_specific.php"
+
   end
 end
